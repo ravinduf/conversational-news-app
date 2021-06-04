7435b31f44e0c3884ca1b8b4dfd3dd3178f0694f
ravinduf <ravinduf@rocketmail.com>
2021-06-04 14:48:48 +0530

    open articles with voice commanda added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
+import wordsToNumbers from 'words-to-numbers';
 
 import NewsCards from './components/NewsCards/NewsCards'
 
@@ -14,13 +15,28 @@ function App() {
   useEffect(() => {
     alanBtn({
       key: alanKey,
-      onCommand: ({ command, articles }) => {
+      onCommand: ({ command, articles, number }) => {
         if(command === 'newHeadlines') {
           setNewsArticles(articles);
           setActiveArticle(0);
         }
         else if (command === 'highlight'){
           setActiveArticle((prev) => prev + 1);
+        }
+        else if (command === 'open'){
+          console.log(number);
+          const parsedNumber = number.length > 2 ? wordsToNumbers(number, { fuzzy: true }) : number;
+          const article = articles[parsedNumber];
+
+          if (parsedNumber > 20){
+            alanBtn().playText('Please try that again')
+          }
+          else if (article) {
+            console.log(article)
+            window.open(article.url, '_blank');
+            alanBtn().playText('Opening..')
+          }
+          
         }
       }
     })
